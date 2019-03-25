296fdc2c291ebd49535667335e5878d2bf8ca997
Nolan Grant <grantnol@shernet.sheridancollege.ca>
2019-03-25 14:19:22 -0400

    BASS

---
+++ Assets/scripts/KingOfTheHillCapturePoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingOfTheHillCapturePoint : MonoBehaviour
+{
+    public GameMode activeGameMode;
+
+    private void OnEnable()
+    {
+        activeGameMode = GetComponentInParent<KingOfTheHillGameMode>();
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("player"))
+        {
+            //colli
+        }
+    }
+}
---
+++ Assets/scripts/KingOfTheHillGameMode.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingOfTheHillGameMode : GameMode
+{
+    public GameObject[] spawnPoints;
+
+    public float[] playerScores;
+
+    public KingOfTheHillCapturePoint capturePoint;
+
+    public override void AtEndOfOnEnableOverride()
+    {
+        SpawnAllPlayers();
+        playerScores = new float[players.Count];
+    }
+
+    public void SpawnAllPlayers()
+    {
+        //always spawn at initial spawn corresponding to ID, never randomly
+        foreach (KeyValuePair<int, Player> player in players)
+        {
+            if (player.Value.activeCharacterInScene == null)
+            {
+                SpawnPlayer(player.Key, spawnPoints[player.Key].transform.position);
+            }
+        }
+    }
+
+    public void RespawnPlayer(int PlayerToRespawnID)
+    {
+        Vector2 furthestAwaySpawnPoint = FindNodeFarthestFromAnyActivePlayer(spawnPoints).transform.position;
+        SpawnPlayer(PlayerToRespawnID, furthestAwaySpawnPoint);
+    }
+
+
+    public override void CharacterCollision(int attackerPlayerID, int VictimPlayerID)
+    {
+        Destroy(players[VictimPlayerID].activeCharacterInScene);
+        RespawnPlayer(VictimPlayerID);
+    }
+
+
+    public void PlayerInCaptureZone()
+    {
+
+    }
+}
