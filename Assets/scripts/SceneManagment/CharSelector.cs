b3dd97ef1484085c58aec04923e8750227d16ca0
Lethaldoren <48733718+Lethaldoren@users.noreply.github.com>
2019-03-25 15:18:54 -0400

    UI Stuff Again

---
+++ Assets/scripts/SceneManagment/CharSelector.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharSelector : MonoBehaviour
+{
+    [Header("Character Manager")]
+    public GameObject[] characters;
+    GameObject currentCharacter;
+
+    public int selectedCharacter = 0;
+
+    public Player player;
+
+    [Header("UIVariables")]
+    public float selectSpriteSize; 
+
+    float selectTime;
+    float timePressed; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (timePressed > selectTime)
+        {
+
+        }
+    }
+
+    void OnButtonDown()
+    {
+
+    }
+
+    void OnButtonUp ()
+    {
+        if (timePressed < selectTime )
+        {
+            timePressed = 0;
+            NextCharacter(); 
+        }
+        
+    }
+
+    void NextCharacter ()
+    {
+        Destroy(currentCharacter);
+
+        selectedCharacter++;
+
+        if (selectedCharacter >= characters.Length )
+        {
+            selectedCharacter = 0; 
+        }
+
+        currentCharacter = Instantiate(characters[selectedCharacter], this.transform);
+        
+        
+    }
+
+
+   IEnumerator OnCharacterSelect ()
+    {
+
+        return null; 
+    }
+}
---
+++ Assets/scripts/SceneManagment/Map.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu (fileName = "unnamedMap")]
+public class Map : ScriptableObject 
+{
+    public string sceneName;
+
+    public GameManager.GameMode[] supportedGameModes;
+
+    public int minPlayers;
+    public int maxPlayers;
+}
